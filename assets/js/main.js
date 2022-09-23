14adf84ecbc4f948799e24de5ad85c2eba8437f9
00tino <fonteviva.valen.tino00@gmail.com>
2022-09-23 17:23:33 +0200

    fixing bugs 1.6

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,21 +6,6 @@
     });
 })()
 
-//const proyecto1 = document.getElementById("proyecto1");
-//const proyecto2 = document.getElementById("proyecto2");
-//progressContainer.addEventListener("click", setProgress)
-
-//function abrirNuevoTab(url) {
-     // Abrir nuevo tab
-        //var win = window.open(url, '_blank');
-    //}
-    //$("#proyecto1").click(function(){
-        //abrirNuevoTab('https://00tino.github.io/mp3Player/')
-    //})
-    //$("#proyecto2").click(function(){
-        //abrirNuevoTab('https://00tino.github.io/mp3Player/')
-    //})
-
 function openInNewTab(url) {
     window.open(url, '_blank').focus();
   }
